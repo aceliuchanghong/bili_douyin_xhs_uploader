296b756761df0d1dbd881da958bd40583bdbffb5
Lawrence-AIP <liuch@allinfinance.com>
2024-01-15 12:42:52 +0800

    修改platforms参数名字

--- README.md
+++ README.md
@@ -5,10 +5,9 @@
 ### Install
 ```
 #pip list --format=freeze > requirements.txt
+git clone https://github.com/aceliuchanghong/bili_douyin_xhs_uploader.git
 conda create -n uploader python=3.9
-
-conda activate worker
-
+conda activate uploader
 pip install -r requirements.txt --proxy=127.0.0.1:10809
 ```
 将对应网址的cookie放在cookies/bili_cookies.json,cookies/xhs_cookies.json,cookies/douyin_cookies.json,或者core/config.py里面修改路径
@@ -47,9 +46,15 @@ bili_douyin_xhs_uploader/
 
 ```shell
 # 判断数据库中是否存在,存在就不上传了
-python main.py --platform xhs --video_url "https://test" --video_path "files/test/11.mp4" --video_name "我只在乎你鄧麗君" --description "我只在乎你鄧麗君 琵琶 演奏"
+# eg:
+python main.py --platforms xhs --video_url "https://test" --video_path "files/test/11.mp4" --video_name "我只在乎你鄧麗君" --description "我只在乎你鄧麗君 琵琶 演奏"
 
-python main.py --platform douyin --video_url "https://test" --video_path "files/test/11.mp4" --video_name "我只在乎你鄧麗君" --description "我只在乎你鄧麗君 琵琶 演奏"
+python main.py --platforms douyin --video_url "https://test" --video_path "files/test/11.mp4" --video_name "我只在乎你鄧麗君" --description "我只在乎你鄧麗君 琵琶 演奏"
+python main.py --platforms douyin --video_url "https://test2" --video_path "files/test/00.mp4" --video_name "你的身边有我们 你的背后是祖国" --description "你的身边有我们 你的背后是祖国"
 
-python main.py --platform bili --video_url "https://test" --video_path "files/test/11.mp4" --video_name "我只在乎你鄧麗君" --description "我只在乎你鄧麗君 琵琶 演奏"
+python main.py --platforms bili --video_url "https://test" --video_path "files/test/11.mp4" --video_name "我只在乎你鄧麗君" --description "我只在乎你鄧麗君 琵琶 演奏"
+```
+### ubuntu环境
+```shell
+python main.py --platforms douyin --video_url "https://test_ubuntu" --video_path "/home/aceliuchanghong/ftpfiles/liu/00.mp4" --video_name "你的身边有我们 你的背后是祖国" --description "你的身边有我们 你的背后是祖国"
 ```
--- main.py
+++ main.py
@@ -5,9 +5,9 @@ from platform.xhs.uploader import XhsUploader
 from utils.util_sqlite import check
 
 """eg:
-python main.py --platform xhs --video_url "https://test" --video_path "files/test/11.mp4" --video_name "我只在乎你鄧麗君" --description "我只在乎你鄧麗君 琵琶 演奏"
-python main.py --platform douyin --video_url "https://test" --video_path "files/test/11.mp4" --video_name "我只在乎你鄧麗君" --description "我只在乎你鄧麗君 琵琶 演奏"
-python main.py --platform bili --video_url "https://test" --video_path "files/test/11.mp4" --video_name "我只在乎你鄧麗君" --description "我只在乎你鄧麗君 琵琶 演奏"
+python main.py --platforms xhs --video_url "https://test" --video_path "files/test/11.mp4" --video_name "我只在乎你鄧麗君" --description "我只在乎你鄧麗君 琵琶 演奏"
+python main.py --platforms douyin --video_url "https://test" --video_path "files/test/11.mp4" --video_name "我只在乎你鄧麗君" --description "我只在乎你鄧麗君 琵琶 演奏"
+python main.py --platforms bili --video_url "https://test" --video_path "files/test/11.mp4" --video_name "我只在乎你鄧麗君" --description "我只在乎你鄧麗君 琵琶 演奏"
 
 """
 # Define a dictionary that maps platform names to their respective uploader classes
@@ -37,7 +37,7 @@ async def main(platform_name, video_url, video_path, video_name, description):
 if __name__ == "__main__":
     # Set up the argument parser
     parser = argparse.ArgumentParser(description="Upload videos to various platforms.")
-    parser.add_argument('--platform', required=True,
+    parser.add_argument('--platforms', required=True,
                         help="The platform to upload the video to (e.g., 'bili', 'douyin', 'xhs').")
     parser.add_argument('--video_url', required=True, help="Url of the video file.")
     parser.add_argument('--video_path', required=True, help="Path to the video file.")
@@ -50,4 +50,4 @@ if __name__ == "__main__":
     # Call the main function with the parsed arguments
     import asyncio
 
-    asyncio.run(main(args.platform, args.video_url, args.video_path, args.video_name, args.description))
+    asyncio.run(main(args.platforms, args.video_url, args.video_path, args.video_name, args.description))
